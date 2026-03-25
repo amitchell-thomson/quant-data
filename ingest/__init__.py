e892fd55fed30079114f1fd493044a66f9809806
Alec Mitchell-Thomson <alec.mitchellthomson@gmail.com>
2026-03-25 00:38:43 +0000

    project-rewrite
    - rewriting a clean new instance of the project to work on home sever
    - removed all old uneeded files
    - created the configs around which the entired data loading will be
    formed

--- ingest/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-EODHD Data Ingestion Pipeline
-
-A production-grade ingestion system for market and fundamental data.
-Designed for survivorship-bias-safe, append-only data collection.
-"""
-
-__version__ = "0.1.0"
